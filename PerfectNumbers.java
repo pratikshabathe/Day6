4630661bdcdcb6bb23ed35b7b8171e50dc3bd6ae
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-07 19:27:52 +0530

    create java file

--- PerfectNumbers.java
+++ PerfectNumbers.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PerfectNumbers {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-		System.out.println("Enter a perfect number");
+		System.out.println("Enter any number");
 		int n = sc.nextInt();
 		sc.close();
 		
@@ -14,9 +14,9 @@ public class PerfectNumbers {
 			if (n % i == 0) factorSum += i;
 		}
 		if (factorSum == n) {
-			System.out.println("Enter a number is perfect number");
+			System.out.println(" it is perfect number");
 		}else {
-			System.out.println("Enter number is not perfect number ");
+			System.out.println("it is not perfect number ");
 		}
 	}
 }
