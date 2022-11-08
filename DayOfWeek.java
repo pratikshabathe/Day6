3cf46710efa933c534ea71be3a1fc4b741561f23
pratikshabathe <pratikshabathe11@gmail.com>
2022-11-08 08:20:31 +0530

    create java files

--- DayOfWeek.java
+++ DayOfWeek.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class DayOfWeek {
 	public static void main(String[] args) {
-		
+
 		int m = 0;
 		Scanner scanner = new Scanner(System.in);
 		boolean keepGoing = true;
@@ -13,10 +13,10 @@ public class DayOfWeek {
 		while(keepGoing1) {
 			System.out.println("Month");
 			m = scanner.nextInt();
-			if(m < 1 || m>=12);
+			if(m < 1 || m>=12) {
 			System.out.println("Months are between 1 and 12");
-			//continue;
-		}
+			}
+			
 			System.out.println("Day");
 			int d = scanner.nextInt();
 			if (d < 1 || d > 31) {
@@ -28,10 +28,11 @@ public class DayOfWeek {
 	       int y = scanner.nextInt();
 	         if (y < -10000 || y > 10000) {
 	            System.out.println("Years are between -10000 and 10000");
-	         //  continue;
+	         
 	         }
-
-	        int y0 = y - (14 - m) / 12;
+		
+	        
+			int y0 = y - (14 - m) / 12;
 	        int x = y0 + y0/4 - y0/100 +y0/400;
 	        int m0 = m + 12 * ((14 - m) / 12) - 2;
 	        int d0 = (d + x + 31 * m0 / 12) % 7;  
@@ -63,10 +64,11 @@ public class DayOfWeek {
 	               }
 	             }
 	             }
-	          } 
+	          }
+		
 
 	        System.out.println("The day of the week is ");
 	    }
 
-	  }
-
+	}
+}
